859aeac9bd5aaf827eff1878c472ac0ffaf84ca9
Hugo Rysak <hugo.rysak4@etu.univ-lorraine.fr>
2024-06-08 08:52:01 +0200

    jesaispas

--- rayTracing/ServiceCalcul/ServiceRayTracer.java
+++ rayTracing/ServiceCalcul/ServiceRayTracer.java
@@ -1,4 +1,4 @@
-package Esclave;
+package ServiceCalcul;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import raytracer.Image;
@@ -7,12 +7,12 @@ import raytracer.Scene;
 public interface ServiceRayTracer extends Remote{
     /**
      * Méthode qui génère et retourne une image
-     * @param x0
-     * @param y0
-     * @param w
-     * @param h
-     * @param c
-     * @return
+     * @param x0 coordonnée x de départ
+     * @param y0 coordonnée y de départ
+     * @param w largeur de l'image généré
+     * @param h hauteur de l'image généré
+     * @param c la Scene avec laquelle généré l'image
+     * @return Image generé
      * @throws RemoteException
      */
     public Image genererImage(int x0, int y0, int w, int h,Scene c)throws RemoteException;
